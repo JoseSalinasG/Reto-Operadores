45b995af4efabc9f88e101c979f0e7380d7ce14e
JoseSalinasG <149660068+JoseSalinasG@users.noreply.github.com>
2023-11-12 21:54:28 -0500

    Add files via upload

---
+++ main1.js
@@ -0,0 +1,49 @@
+// Variables
+let número1 = 14 ;
+let número2 = 12 ;
+let texto1 = 'Hola Hundo';
+let texto2 = 'Hola Universo';
+let objeto = { 
+    nombre: 'Jose',
+    edad: 25
+ };
+let objeto1 = {
+    nombre: 'Jose',
+    edad: 25 
+ };
+let dato1 = 10;
+let dato2 = 3;
+// Constantes
+const text = '¿El número 1 es mayor que el número 2?';
+const text0 = '¿El número 2 es menor que el número 1?';
+const text1 = '¿El número 1 es igual número 2?';
+const text2 = '¿El texto 1 es igual al texto 2?';
+const text3 = '¿Los objetos son iguales?';
+const text4 = 'AND lógico:';
+const text5 = 'OR lógico:';
+const text6 = 'La suma de 10 + 3:';
+const text7 = 'La resta de 10 - 3:';
+const text8 = 'La multiplicación de 10 * 3:';
+const text9 = 'La división de 10/3:';
+const text10 ='10 módulo de 3:';
+
+// Operaciones
+console.log(text, número1 > número2);
+console.log(text0, número1 < número2);
+console.log(text1, número1 == número2);
+console.log(text2,text1 === text2);
+console.log(text3,objeto === objeto1);
+console.log(text4, número1 > 0 && número2 < 0);
+console.log(text5, número1 > 0 || número2 < 0);
+console.log(text6,dato1 + dato2);
+console.log(text7,dato1 - dato2);
+console.log(text8,dato1 * dato2);
+console.log(text9,dato1 / dato2);
+console.log(text10,dato1 % dato2);
+
+
+
+
+
+
+
